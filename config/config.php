c093e1d552ec8bbf2801c701f0ce864574c3a9ea
soumalya-droid <ssaha.assay@gmail.com>
2025-09-09 22:06:22 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,9 +3,9 @@
 // Copy this file as-is and set your DB credentials
 return [
   'db_host' => getenv('DB_HOST') ?: 'localhost',
-  'db_name' => getenv('DB_NAME') ?: 'u269008503_influencermart',
-  'db_user' => getenv('DB_USER') ?: 'u269008503_system',
-  'db_pass' => getenv('DB_PASS') ?: '6pnaW:Rz$',
+  'db_name' => getenv('DB_NAME') ?: 'u269008503_infmrt_test',
+  'db_user' => getenv('DB_USER') ?: 'u269008503_db_admin',
+  'db_pass' => getenv('DB_PASS') ?: 'mEmd$@M$9',
   'base_url' => '/', // adjust if in subfolder, e.g., '/influencer/'
   'session_name' => 'influ_sess',
   'app_name' => 'InfluenceHub'
